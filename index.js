f16ec71ac1522bf42cac992e18de33345193b539
Sergei Krivochenko <admin@krivochenko.ru>
2024-06-14 16:36:37 -0300

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 console.log(window.Telegram.WebApp);
 
-// window.Telegram.WebApp.onEvent('viewportChanged', function(isStateStable) {
-//     console.log('viewportChanged', isStateStable);
-// });
+let scrolled = false
+
+window.Telegram.WebApp.onEvent('viewportChanged', function(isStateStable) {
+    if (!scrolled && isStateStable) {
+        window.scrollTo(0, 100);
+    }
+});
+
 window.Telegram.WebApp.ready();
 window.Telegram.WebApp.setBackgroundColor('#ff0000');
 window.Telegram.WebApp.setHeaderColor('#ff0000');
@@ -10,16 +15,6 @@ window.Telegram.WebApp.expand();
 
 window.Telegram.WebApp.MainButton.setText('Test');
 window.Telegram.WebApp.MainButton.show();
-
-
-
-const overflow = 100;
-
-// document.body.style.overflowY = 'hidden'
-// document.body.style.marginTop = `${overflow}px`
-// document.body.style.height = window.innerHeight + overflow + "px"
-// document.body.style.paddingBottom = `${overflow}px`
-window.scrollTo(0, overflow);
 
 window.Telegram.WebApp.MainButton.onClick(function() {
     const current = parseInt(document.getElementById('counter').textContent);
