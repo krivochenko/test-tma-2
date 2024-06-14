9f127d70013b5b8e779cef98cbb9b5564d2df354
Sergei Krivochenko <admin@krivochenko.ru>
2024-06-14 15:39:42 -0300

    Fix

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ window.Telegram.WebApp.ready();
 window.Telegram.WebApp.MainButton.setText('Test');
 window.Telegram.WebApp.MainButton.show();
 
+const overflow = 100
+document.body.style.overflowY = 'hidden'
+document.body.style.marginTop = `${overflow}px`
+document.body.style.height = window.innerHeight + overflow + "px"
+document.body.style.paddingBottom = `${overflow}px`
+window.scrollTo(0, overflow);
+
 window.Telegram.WebApp.MainButton.onClick(function() {
     const current = parseInt(document.getElementById('counter').textContent);
     document.getElementById('counter').textContent = (current + 1).toString();
