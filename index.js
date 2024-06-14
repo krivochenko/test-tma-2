7515b5768484bfd3faf72c71cbd8a011d43764e7
Sergei Krivochenko <admin@krivochenko.ru>
2024-06-14 16:27:03 -0300

    Revert "Fix"
    This reverts commit 2c4a89182645a6418b2dd48bce5c0ddbcfd9e996.

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const overflow = 100;
 
 // document.body.style.overflowY = 'hidden'
 // document.body.style.marginTop = `${overflow}px`
-document.body.style.height = window.innerHeight + overflow + "px"
-document.body.style.paddingBottom = `${overflow}px`
+// document.body.style.height = window.innerHeight + overflow + "px"
+// document.body.style.paddingBottom = `${overflow}px`
 window.scrollTo(0, overflow);
 
 window.Telegram.WebApp.MainButton.onClick(function() {
