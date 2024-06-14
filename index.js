7d3e0ad67f2e48f33b5558d9a0ce0e8dc40c3221
Sergei Krivochenko <admin@krivochenko.ru>
2024-06-14 15:18:29 -0300

    Fix

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ console.log(window.Telegram.WebApp);
 
 window.Telegram.WebApp.ready();
 
-<button onClick={() => window.Telegram.WebApp.setParams({ color: '#00ff00' })}>Change color</button>
-
 window.Telegram.WebApp.MainButton.setText('Test');
 window.Telegram.WebApp.MainButton.show();
