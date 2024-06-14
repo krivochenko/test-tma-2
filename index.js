78390784c8c8c1ae6507586a11801d6672509fa6
Sergei Krivochenko <admin@krivochenko.ru>
2024-06-14 16:53:11 -0300

    Fix

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ window.Telegram.WebApp.expand();
 window.Telegram.WebApp.MainButton.setText('Test');
 window.Telegram.WebApp.MainButton.show();
 
+setTimeout(() => {
+    document.getElementById('content').style.display = 'block';
+}, 2000);
+
 window.Telegram.WebApp.MainButton.onClick(function() {
     const current = parseInt(document.getElementById('counter').textContent);
     document.getElementById('counter').textContent = (current + 1).toString();
